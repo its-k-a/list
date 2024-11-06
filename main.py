8b84eee18a3a101cedbe8e7928da57bf93fc5180
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-11-06 09:31:47 -0500

    1st

---
+++ main.py
@@ -0,0 +1,17 @@
+#SECTION #1
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+print(color_list[-1])
+
+#SECTION #2
+num_list = [4, 6, 4, 2, 9, 4, 1]
+num_list.append(5)
+print(num_list[:4])
+print(num_list.count(4))
+print(num_list.index(9))
+
+#SECTION #3
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+print(fruit_list)
+fruit_list.pop()
